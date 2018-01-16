0971d399b13331204ee6a4ef98497174ee3e04d4
unknown <tji132006@gmail.com>
2018-01-15 21:23:00 -0800

    add jump after registration

--- src/components/Register.js
+++ src/components/Register.js
@@ -30,9 +30,10 @@ class RegistrationForm extends React.Component {
                         username: values.username,
                         password: values.password
                     })
-                }).then(function(response) {
+                }).then((response) => {
+                    this.props.history.push('/login');
                     message.success(response);
-                }).catch(function(error) {
+                }).catch((error) =>{
                     message.error(error.responseText);
                 });
             }
