0d2ee63226fed84c870a26d980bed0d1b77ceffb
unknown <tji132006@gmail.com>
2018-01-15 22:07:27 -0800

    add localStorage

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import {Header} from './Header';
 import {Main} from './Main';
 import '../styles/App.css';
-
+import {TOKEN_KEY} from "../constants"
 
 
 class App extends React.Component {
+    state = {
+        isLoggedIn: !!localStorage.getItem(TOKEN_KEY)
+    }
+
+    loginHandler = (response) => {
+        localStorage.setItem(TOKEN_KEY, response)
+        this.setState({isLoggedIn: true});
+    }
   render() {
     return (
       <div className="App">
         <Header/>
-        <Main/>
+        <Main isLoggedIn = {this.state.isLoggedIn} loginHandler = {this.loginHandler}/>
       </div>
     );
   }
--- src/components/Main.js
+++ src/components/Main.js
@@ -1,18 +1,27 @@
 import React from 'react';
 import {Register} from './Register';
 import { Switch, Route } from 'react-router-dom'
+import {Home} from "./Home"
 
 import {Login} from './Login';
 
 export class Main extends React.Component{
+   getLogin = () => {
+       return this.props.isLoggedIn ? <Home/> : <Login loginHandler = {this.props.loginHandler}/>
+   }
+
+   getHome = () => {
+       return this.props.isLoggedIn ? <Home/> :<Login loginHandler = {this.props.loginHandler}/>
+   }
     render(){
         return (
             <div className='main'>
                 <Switch>
-                    <Route exact path="/" component={Login}/>
+                    <Route exact path="/" render={this.getLogin}/>
                     <Route path="/register" component={Register}/>
-                    <Route path="/login" component={Login}/>
-                    <Route component={Login}/>
+                    <Route path="/login" render={this.getLogin}/>
+                    <Route path = "/home" render = {this.getHome}/>
+                    <Route render={this.getLogin}/>
                 </Switch>
             </div>
         );
