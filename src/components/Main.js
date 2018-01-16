bd77290761e156e158dc74dad16ee9efcf378ed1
unknown <tji132006@gmail.com>
2018-01-15 22:18:03 -0800

    fix all url links

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import {Register} from './Register';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import {Home} from "./Home"
 
 import {Login} from './Login';
 
 export class Main extends React.Component{
    getLogin = () => {
-       return this.props.isLoggedIn ? <Home/> : <Login loginHandler = {this.props.loginHandler}/>
+       return this.props.isLoggedIn ? <Redirect to="/home"/> : <Login loginHandler = {this.props.loginHandler}/>;
    }
 
    getHome = () => {
-       return this.props.isLoggedIn ? <Home/> :<Login loginHandler = {this.props.loginHandler}/>
+       return this.props.isLoggedIn ? <Home/> :<Redirect to="/login"/>;
+   }
+
+   getRoot = () => {
+       return <Redirect to = "/login"/>
    }
     render(){
         return (
             <div className='main'>
                 <Switch>
-                    <Route exact path="/" render={this.getLogin}/>
+                    <Route exact path="/" render={this.getRoot}/>
                     <Route path="/register" component={Register}/>
                     <Route path="/login" render={this.getLogin}/>
                     <Route path = "/home" render = {this.getHome}/>
-                    <Route render={this.getLogin}/>
+                    <Route render={this.getRoot}/>
                 </Switch>
             </div>
         );
