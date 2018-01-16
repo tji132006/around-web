65dc643b1d03f051ee92927914b77f70e4f595d7
unknown <tji132006@gmail.com>
2018-01-15 19:47:06 -0800

    login form ready

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import {Register} from './Register';
 
+import {Login} from './Login';
+
 export class Main extends React.Component{
     render(){
         return (
-            <div className='main'><Register/></div>
+            <div className='main'>
+
+                <Login/>
+            </div>
         );
     }
 }
