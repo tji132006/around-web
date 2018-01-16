886884f95bc13b312f50a6ad856e5e46daf31f41
unknown <tji132006@gmail.com>
2018-01-15 21:14:29 -0800

    add login request

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,6 +1,9 @@
 import React from 'react';
-import { Form, Icon, Input, Button, Checkbox } from 'antd';
+import { Form, Icon, Input, Button, message } from 'antd';
 import { Link } from 'react-router-dom'
+import {API_ROOT} from "../constants"
+import $ from 'jquery';
+
 const FormItem = Form.Item;
 
 class NormalLoginForm extends React.Component {
@@ -9,6 +12,20 @@ class NormalLoginForm extends React.Component {
         this.props.form.validateFields((err, values) => {
             if (!err) {
                 console.log('Received values of form: ', values);
+                $.ajax({
+                    url: `${API_ROOT}/login`,
+                    method: 'POST',
+                    data: JSON.stringify({
+                        username: values.username,
+                        password: values.password,
+                    })
+                }).then((response) => {
+                    message.success(response);
+                }, (error) => {
+                    message.error(error.responseText);
+                }).catch((error) => {
+                    console.log(error);
+                });
             }
         });
     }
@@ -17,7 +34,7 @@ class NormalLoginForm extends React.Component {
         return (
             <Form onSubmit={this.handleSubmit} className="login-form">
                 <FormItem>
-                    {getFieldDecorator('userName', {
+                    {getFieldDecorator('username', {
                         rules: [{ required: true, message: 'Please input your username!' }],
                     })(
                         <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder="Username" />
