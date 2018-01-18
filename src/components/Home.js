8d777f6ac6ebd582bc1115341af7679f199c0a46
unknown <tji132006@gmail.com>
2018-01-17 20:33:15 -0800

    loading geo location finish

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Tabs, Button } from 'antd';
 import {GEO_OPTIONS} from "../constants";
+import { Spin, Alert } from 'antd';
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
@@ -8,9 +9,10 @@ const operations = <Button>Extra Action</Button>;
 export class Home extends React.Component{
     state = {
         loadingGeoLocation: false,
+        error: '',
     }
     componentDidMount() {
-        this.setState({loadingGeoLocation: true})
+        this.setState({loadingGeoLocation: true , error: ''})
         this.getGeoLocation();
     }
 
@@ -25,25 +27,34 @@ export class Home extends React.Component{
 
         } else {
             /* geolocation IS NOT available */
+            this.setState({error: 'Your broswer does not support geoLocation'})
         }
     }
 
     onSuccessLoadGeoLocation = (position) =>{
         console.log(position);
-        this.setState({loadingGeoLocation: false})
+        this.setState({loadingGeoLocation: false, error: ''})
     }
 
     onFailedLoadGeoLocation = () =>{
-        this.setState({loadingGeoLocation: false})
+        this.setState({loadingGeoLocation: false, error: 'Failed to load geoLocation!'})
+    }
+
+    getGalleryPanelContent = () => {
+        if(this.state.error){
+            return <div>{this.state.error}</div>
+        } else if (this.state.loadingGeoLocation){
+            return <Spin tip="Loading geo location..." />;
+        } else {
+            return null;
+        }
     }
     render(){
         return(
 
                 <Tabs tabBarExtraContent={operations} className = "main-tabs">
                     <TabPane tab="Posts" key="1">
-                        {this.state.loadingGeoLocation ?
-                            <span>loading geo location</span> : null
-                        }
+                        {this.getGalleryPanelContent()}
                     </TabPane>
                     <TabPane tab="Map" key="2">Content of tab 2</TabPane>
                 </Tabs>
