75fc6a560e48f530a5dede815e331e11c0fae36c
unknown <tji132006@gmail.com>
2018-01-22 20:31:09 -0800

    new post modal finsh

--- src/components/CreateButtonForm.js
+++ src/components/CreateButtonForm.js
@@ -1,9 +1,16 @@
 import React from 'react';
-import { Form, Input} from 'antd';
+import { Form, Input, Upload, Icon} from 'antd';
 
 const FormItem = Form.Item;
 
 class CreatePostForm extends React.Component{
+    normFile = (e) => {
+        console.log('Upload event:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e && e.fileList;
+    }
 
     render(){
         const { getFieldDecorator } = this.props.form;
@@ -21,6 +28,26 @@ class CreatePostForm extends React.Component{
                     })(
                         <Input />
                     )}
+                </FormItem>
+                <FormItem
+                    {...formItemLayout}
+                    label="Image"
+                >
+                    <div className="dropbox">
+                        {getFieldDecorator('image', {
+                            valuePropName: 'fileList',
+                            getValueFromEvent: this.normFile,
+                            rules: [{ required: true, message: 'Please select an image' }],//校验用
+                        })(
+                            <Upload.Dragger name="files" action="/upload.do">
+                                <p className="ant-upload-drag-icon">
+                                    <Icon type="inbox" />
+                                </p>
+                                <p className="ant-upload-text">Click or drag file to this area to upload</p>
+                                <p className="ant-upload-hint">Support for a single or bulk upload.</p>
+                            </Upload.Dragger>
+                        )}
+                    </div>
                 </FormItem>
             </Form>
         );
