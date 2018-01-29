690bc2d73c3d890dd739da0b3daaf4936a99172c
unknown <tji132006@gmail.com>
2018-01-29 00:51:58 -0800

    seperate around marker

--- src/components/AroundMap.js
+++ src/components/AroundMap.js
@@ -1,42 +1,26 @@
 import React from 'react';
 import {POS_KEY} from "../constants"
-const {
+import {AroundMarker} from "./AroundMarker";
+
+import {
     withScriptjs,
     withGoogleMap,
     GoogleMap,
-    Marker,
-    InfoWindow,
-} = require("react-google-maps");
+} from "react-google-maps";
 
 class AroundMap extends React.Component{
-    state = {
-        isOpen: false,
-    }
-    onToggleOpen = () => {
-        this.setState(
-            (prevState) => ({isOpen: !prevState.isOpen})
-        );
-    }
+
     render(){
         const {lat, lon} = JSON.parse(localStorage.getItem(POS_KEY));
         return(
             <GoogleMap
-                defaultZoom={8}
+                defaultZoom={11}
                 defaultCenter={{ lat: lat, lng: lon }}
             >
                 {
                     this.props.posts.map((post) =>
-                        <Marker
-                        position={{ lat: post.location.lat, lng: post.location.lon }}
-                        onClick={this.onToggleOpen}
-                        key = {post.url}
-                        >
-                        {this.state.isOpen && <InfoWindow onCloseClick={this.onToggleOpen}>
-                            <div>text</div>
-                        </InfoWindow>}
-                        </Marker>
-
-                )
+                        <AroundMarker post = {post} key = {post.url}/>
+                    )
                 }
 
 
